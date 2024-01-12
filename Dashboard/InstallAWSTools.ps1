196055376d08c08f4a2ab9d05ce56ecbf2390b69
MagicalClicks <76107003+MagicalClicks@users.noreply.github.com>
2024-01-12 16:06:22 +0000

    Update InstallAWSTools.ps1

--- Dashboard/InstallAWSTools.ps1
+++ Dashboard/InstallAWSTools.ps1
@@ -1,14 +1,5 @@
-# This script checks and installs aws.tools.installer and aws.tools.common modules 
-# and any other module specfied in the $RequiredModules list
 
-# Create a list of all modules already installed and 
-# then identify if the required modules are already installed. Read Idempotent.
 $ListofModulesInstalled = (Get-InstalledModule).Name
-# These are the modules being installed:
-# https://www.powershellgallery.com/packages/AWS.Tools.Installer
-# https://www.powershellgallery.com/packages/AWS.Tools.Common
-# Populate below list with modules based on your requirement
-# More modules could be found at: https://www.powershellgallery.com/packages?q=AWS.Tools
 $RequiredModules = @(
                         "AWS.Tools.Installer",
                         "AWS.Tools.Common",
