e1cce81136d3c4e5c6361fea2ee534ec96097ff0
MagicalClicks <76107003+MagicalClicks@users.noreply.github.com>
2024-01-12 16:25:03 +0000

    Update variables.tf

--- Workspace/variables.tf
+++ Workspace/variables.tf
@@ -1,16 +1,13 @@
-#Define AWS Region
 variable "region" {
   description = "Infrastructure region"
   type        = string
   default     = "eu-west-1"
 }
-#Define IAM User Access Key
 variable "access_key" {
   description = "The access_key that belongs to the IAM user"
   type        = string
   sensitive   = true
 }
-#Define IAM User Secret Key
 variable "secret_key" {
   description = "The secret_key that belongs to the IAM user"
   type        = string
